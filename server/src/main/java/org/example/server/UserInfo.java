4b987844e05ab488257af4ee5cbe966746493c71
Thomas Segismont <tsegismo@redhat.com>
2024-09-23 18:15:22 +0200

    Update backend pages
    Make it look like the original implementation in https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authenticating-to-the-rest-api-with-an-oauth-app
    
    Signed-off-by: Thomas Segismont <tsegismo@redhat.com>

--- server/src/main/java/org/example/server/UserInfo.java
+++ server/src/main/java/org/example/server/UserInfo.java
@@ -1,6 +1,13 @@
 package org.example.server;
 
+import io.quarkus.qute.TemplateData;
+
 import java.util.List;
 
+@TemplateData
 public record UserInfo(String login, String email, List<String> privateEmails) {
+
+    public String privateEmailsAsString() {
+        return String.join(",", privateEmails);
+    }
 }
